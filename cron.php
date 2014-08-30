ff9c3052b7b91f98c2615cd1c5c34565220e8bf6
cdhassell <cdhassell@gmail.com>
2014-08-29 20:03:01 -0400

    Cleanup of cron.php after testing.

--- cron.php
+++ cron.php
@@ -1,8 +1,11 @@
-#!/usr/local/bin/php
+#!/usr/bin/php5
 <?php namespace freeseat;
 
 // import wordpress stuff
 require_once( '../../../wp-load.php' );
+
+// All paths in FreeSeat are relative to FS_PATH
+//define( 'FS_PATH', plugin_dir_path( __FILE__ ) );
 
 // import freeseat stuff
 require_once (FS_PATH . "vars.php");
@@ -26,7 +29,6 @@ if ( is_array($freeseat_vars) ) {
 	}
 }
 
-
 /** Copyright (C) 2010 Maxime Gamboni. See COPYING for
 copying/warranty info.
 
@@ -41,78 +43,77 @@ $Id: cron.php 388 2012-03-30 21:07:47Z tendays $
  **/
 
 if (isset($_SERVER["REQUEST_METHOD"]) && !$unsecure_login) {
-  db_connect(); // (just to make sure the admin state is written properly)
-  fatal_error($lang["err_shellonly"]);
+	db_connect(); // (just to make sure the admin state is written properly)
+	fatal_error($lang["err_shellonly"]);
 } else {
 
-  prepare_log("cronjob");
+	prepare_log("cronjob");
 
-  /* 1 - clean up seat_locks */
+	/* 1 - clean up seat_locks */
 
-  $q = "delete from seat_locks where until < ".$now;
-  print $q."\n";
-  freeseat_query($q);
-  $q = "delete from booking where state=".ST_DELETED." and firstname='Disabled' and lastname='Seat'";
-  print $q."\n";
-  freeseat_query($q);
+	$q = "delete from seat_locks where until < ".$now;
+	print $q."\n";
+	freeseat_query($q);
+	$q = "delete from booking where state=".ST_DELETED." and firstname='Disabled' and lastname='Seat'";
+	print $q."\n";
+	freeseat_query($q);
 
-  /* 2 - send reminders+cancellation notices */
+	/* 2 - send reminders+cancellation notices */
 
-  $c = get_config();
+	$c = get_config();
 
-  start_notifs();
+	start_notifs();
 
-  $del_count = 0;
-  $shake_count = 0;
+	$del_count = 0;
+	$shake_count = 0;
 
-  /* We use strict timestamp comparison because it works on dates. So
- e.g. exactly paydelay+1 days after the day it was made on, the
- booking is cancelled. The +1 is because cron is meant to be ran
- shortly *after* midnight. */
+	/* We use strict timestamp comparison because it works on dates. So	
+	e.g. exactly paydelay+1 days after the day it was made on, the
+	booking is cancelled. The +1 is because cron is meant to be ran
+	shortly *after* midnight. */
 
-  //  $now = time();
-    /** First delete very old bookings (note, $c["Xdelay_Y"] are days
-     * so we multiply by number of seconds in a day **/
-  foreach (array(PAY_CCARD => date("Y-m-d H:i:s",$now-86400*$c["paydelay_ccard"]),
-		 PAY_POSTAL => date("Y-m-d H:i:s",sub_open_time($now,86400*$c["paydelay_post"]))) as $val => $dl) {
-    /*    echo "\ndeleting\n";
-    echo ("state=".ST_SHAKEN." and '$dl' > timestamp and payment=$val"); */
-    $toexpire = get_bookings("state=".ST_SHAKEN." and '$dl' > timestamp and payment=$val","shows.date,shows.time,booking.id");
+	//  $now = time();
+	/** First delete very old bookings (note, $c["Xdelay_Y"] are days
+	 * so we multiply by number of seconds in a day **/
+	foreach (array(PAY_CCARD => date("Y-m-d H:i:s",$now-86400*$c["paydelay_ccard"]),
+		PAY_POSTAL => date("Y-m-d H:i:s",sub_open_time($now,86400*$c["paydelay_post"]))) as $val => $dl) {
+		echo "\ndeleting\n";
+		echo ("state=".ST_SHAKEN." and '$dl' > timestamp and payment=$val");
+		$toexpire = get_bookings("state=".ST_SHAKEN." and '$dl' > timestamp and payment=$val", "shows.date, shows.time, booking.id");
 
-    if ($toexpire) {
-    foreach ($toexpire as $n => $bk) {
-      set_book_status($bk,ST_DELETED);
-      $del_count ++;
-    }
-  }
-  }
+		if ($toexpire) {
+			foreach ($toexpire as $n => $bk) {
+				set_book_status($bk,ST_DELETED);
+				$del_count ++;
+			}
+		}
+	}
 
-    /** Now for bookings that have not been deleted, shake the ones that
-are fairly old */
-  foreach (array(PAY_CCARD => date("Y-m-d H:i:s",$now-86400*$c["shakedelay_ccard"]),
-		 PAY_POSTAL => date("Y-m-d H:i:s",sub_open_time($now,86400*$c["shakedelay_post"]))) as $val => $dl) {
-    /*    echo "\nshaking\n";
-    echo ("state=".ST_BOOKED." and '$dl' > timestamp and payment=$val"); */
-    $toshake = get_bookings("state=".ST_BOOKED." and '$dl' > timestamp and payment=$val","shows.date,shows.time,booking.id");
-    if ($toshake) {
-    foreach ($toshake as $n => $bk) {
-      set_book_status($bk,ST_SHAKEN);
-      $shake_count ++;;
-    }
-    }
-  }
-  $mail_count = send_notifs();
+	/** Now for bookings that have not been deleted, shake the ones that are fairly old */
+	foreach (array(PAY_CCARD => date("Y-m-d H:i:s",$now-86400*$c["shakedelay_ccard"]),
+		PAY_POSTAL => date("Y-m-d H:i:s",sub_open_time($now,86400*$c["shakedelay_post"]))) as $val => $dl) {
+		echo "\nshaking\n";
+		echo ("state=".ST_BOOKED." and '$dl' > timestamp and payment=$val");
+		$toshake = get_bookings("state=".ST_BOOKED." and '$dl' > timestamp and payment=$val", "shows.date, shows.time, booking.id");
+		if ($toshake) {
+			foreach ($toshake as $n => $bk) {
+				set_book_status($bk,ST_SHAKEN);
+				$shake_count ++;;
+			}
+		}
+	}
+	$mail_count = send_notifs();
 
-  if ($del_count)
-    echo "delete $del_count ";
-  if ($shake_count)
-    echo "shake $shake_count ";
-  if ($mail_count)
-    echo "mail $mail_count";
+	if ($del_count)
+		echo "delete $del_count ";
+	if ($shake_count)
+		echo "shake $shake_count ";
+	if ($mail_count)
+		echo "mail $mail_count";
 
-  do_hook('cron');
-  echo "\nDone.\n";
-  log_done();
+	do_hook('cron');
+	echo "\nDone.\n";
+	log_done();
 }
 
 ?>
