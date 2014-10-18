366963e8212f013e7995f945bf2cbf66cf3ae201
cdhassell <cdhassell@gmail.com>
2014-10-18 14:03:07 -0400

    Improved text for user ticket page.

--- plugins/login/languages/default.php
+++ plugins/login/languages/default.php
@@ -7,4 +7,4 @@ $lang['login_required'] = "Login Required";
 $lang['login_reminder'] = "Please log in before making a ticket purchase";
 $lang['login_start_typing'] = "Start typing the first few letters of the name until your selection appears";
 $lang['login_user_search'] = "User Search";
-$lang['login_greeting'] = "On this page you may view your recent purchases, reprint your tickets or make a payment for any unpaid reservations.";
+$lang['login_greeting'] = "On this page you may view your recent purchases, reprint your tickets or make a payment for any unpaid reservations.  Action links will appear as you hover over each row.  To select more than one row, use the checkboxes at the left side, and then choose a \"Bulk Action\" from the list at the top.";
