2554fe6a87bfb842e1f514e60e2650a795a96ffc
cdhassell <cdhassell@gmail.com>
2015-06-28 20:46:19 -0400

    Adding postal plugin

---
+++ plugins/postal/info.php
@@ -0,0 +1,11 @@
+<?php namespace freeseat;
+
+function postal_info() {
+    return array
+	('english_name' => 'Swiss Postal Payments',
+	 'version' => '2.0',
+	 'required_fs_version' => '1.4.0',
+	 'category' => 'payment',
+	 'summary' => 'Enables Swiss Postal Payments.',
+	 'details' => 'Enables Swiss Postal Payments.');
+}
---
+++ plugins/postal/languages/default.php
@@ -0,0 +1,11 @@
+<?php namespace freeseat;
+
+$lang["paypal_failure_page"] = "<h3>We're sorry, but your payment was not completed.</h3>
+<p class='main'>You may try again, or contact the office for assistance.</p>
+<p class='main'>Thank you for your patience.</p><br>
+<a href='%s'>Click here to try again</a>"; 
+
+$lang["paypal_button_text"] = 'Finish Ticket Purchase';
+$lang["paypal_thanks"] = 'Thank You';
+$lang["paypal_sorry"] = 'Sorry';
+
